ebae7e98df01704152d8e1938f0158a085522d10
henningboat <talktohenning@gmail.com>
2021-01-29 21:24:34 +0100

    tweaked player

---
+++ Assets/Code/Runtime/Gameplay/LocalPlayerMarker.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace PlanetNein.Runtime.Gameplay
+{
+    public class LocalPlayerMarker : MonoBehaviour
+    {
+        private void Start()
+        {
+            gameObject.SetActive(GetComponentInParent<PhotonView>().IsMine);
+        }
+    }
+}
